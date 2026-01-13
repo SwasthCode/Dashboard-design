05d50e77af13b36960450bc5d8021f36bdaf5b29
the-anshu-dev <anshuchaurasia210@gmail.com>
2026-01-13 21:37:28 +0530

    -

--- src/pages/AuthPages/AuthPageLayout.tsx
+++ src/pages/AuthPages/AuthPageLayout.tsx
@@ -9,6 +9,15 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
+
+
+
+
+
+
+
+
+    
     <div className="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
       <div className="relative flex flex-col justify-center w-full h-screen lg:flex-row dark:bg-gray-900 sm:p-0">
         {children}
